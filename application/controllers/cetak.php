6dc56c63f8fdca9a57679e9ab7e2b8a63ba97368
yudhaazim <mendexjiroh@gmail.com>
2020-10-27 23:41:32 +0800

    praktikum bootstrap 27/10

--- application/controllers/cetak.php
+++ application/controllers/cetak.php
@@ -1,8 +1,14 @@
 <?php
 
 class cetak extends CI_Controller {
-	public function index ()
+	public function index($url_nim='',$url_jur='')
 	{
-	echo "COBA CONTROLLER";
+		$data['nama'] = 'Yudha';
+		$data['umur'] = '17';
+		$data['nim'] = $url_nim;
+		$data['jurusan'] = $url_jur;
+
+	$this->load->view('mahasiswa/v_index', $data);
+
 	}
 }
---
+++ application/views/mahasiswa/v_index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DATA MAHASISWA</title>
+	<link rel="stylesheet"
+		href="<?= base_url('assets/bootstrap/css/bootstrap.min.css'); ?>">
+</head>
+<body>
+<h2>DATA MAHASISWA</h2>
+<p>Nama : <?php echo $nama; ?> </p>
+<p>Umur : <?php echo $umur; ?> </p>
+<p>NIM : <?php echo $nim; ?> </p>
+<p>Jurusan : <?php echo $jurusan; ?> </p>
+</body>
+</html>
